fae44300a55ec45717c8a2669b5e4e66ae8c3a36
Ali zaib <129104458+AlizaibWEBdev@users.noreply.github.com>
2025-08-08 16:47:14 +0500

    add path

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const PORT = process.env.PORT || 3000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-app.set("views", __dirname + "/views");
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the 'public' directory
-app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use(express.json());
